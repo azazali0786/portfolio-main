87a559c77415cf507517658f8bdaf42813fc71f4
azazali786 <azazali786abc@gmail.com>
2025-12-25 20:47:58 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
@@ -9,11 +10,19 @@ import Clients from "@/components/Clients";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
+const FloatingNav = dynamic(
+  () => import("@/components/ui/FloatingNavbar").then((mod) => mod.FloatingNav),
+  { ssr: false }
+);
 
 const Home = () => {
   return (
-  <main id="home" style={{ scrollMarginTop: "6rem" }} className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main
+      id="home"
+      style={{ scrollMarginTop: "6rem" }}
+      className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"
+    >
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
